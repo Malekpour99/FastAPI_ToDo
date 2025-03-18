a1268beaa222f876d5434167a4ed3180ee145192
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-18 16:46:37 +0330

    feat: main - get single to do task using ID
    Created a new view in order to return to do tasks based on their ID

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import List, Annotated
 
-from fastapi import FastAPI, Depends
+from starlette import status
 from sqlalchemy.orm import Session
+from fastapi import FastAPI, Path, Depends, HTTPException
 
 from models import Base, Todos
 from database import engine, SessionLocal
@@ -26,8 +27,16 @@ def get_db():
 # Dependency injection
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all_todos(db: db_dependency):
     """Returns all the to-do tasks from the database"""
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}/", status_code=status.HTTP_200_OK)
+async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    """Returns desired to do task based on its ID"""
+    todo_task = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_task:
+        return todo_task
+    raise HTTPException(status_code=404, detail="Not found")
 
