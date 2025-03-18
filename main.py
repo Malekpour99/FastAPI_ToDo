9dacfe78b2fb9431d1505eb5915fc4d3cb75b1d7
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-18 13:42:03 +0330

    feat: main - reading all tasks
    Created a database session object for dependency injection
    and created a view for returning all the database tasks

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from fastapi import FastAPI
+from typing import List, Annotated
 
-import models
-from database import engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from models import Base, Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+# Creating database if it doesn't exist
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    """
+    Connects to the database 
+    then closes its connection after use
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Dependency injection
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all_todos(db: db_dependency):
+    """Returns all the to-do tasks from the database"""
+    return db.query(Todos).all()
 
