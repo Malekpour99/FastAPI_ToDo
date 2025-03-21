371f1682edf46af6d8f36a820d2829f9b8527907
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-21 13:38:57 +0330

    feat: models - Created Users model
    Created Users model for managing users and created a FK relation between Todos and Users for managing each users' to do separately

--- models.py
+++ models.py
@@ -1,6 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 from database import Base
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    
 
 class Todos(Base):
     __tablename__ = "todos"
@@ -10,4 +23,5 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner = Column(Integer, ForeignKey("users.id"))
 
