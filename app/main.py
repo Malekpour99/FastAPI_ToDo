60b362eb2f707a6d63fbadb152d0dc742613a03b
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-22 15:21:46 +0330

    fix: main routers - users router
    Fixed importing auth router by changing it to users and completely removed auth router from project which was replaced by users router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routers import auth, todos
+from routers import users, todos
 from db.database import Base, engine
 
 # Creating database if it doesn't exist
@@ -8,6 +8,6 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(auth.router)
+app.include_router(users.router)
 app.include_router(todos.router)
 
