74c2d00997fe014b983d5ea3261b2e5f2febfc4a
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-23 12:18:51 +0330

    ref: db - database URL variable
    Removed hardcoded database URL path and used database URL from the project's core config

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo-app.db"
+from core.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(
             SQLALCHEMY_DATABASE_URL,
