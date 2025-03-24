6e1be5e263f7552534f085bb2c15aafbeb050c45
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-24 14:07:36 +0330

    feat: config - access token expirtation
    Created an environment variable for managing access token expiration time

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Database
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+# JWT
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
