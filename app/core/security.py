e48ccdcfd07bf1c31999b31b3537a0ca3103b14c
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-24 16:26:00 +0330

    feat: security - get current user from token
    Created a new security service which will decode and get the current user from the provided JWT access token

--- app/core/security.py
+++ app/core/security.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 from jose import jwt, JWTError
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import status, Depends, HTTPException
 
 from models.users import Users
 from dependencies import db_dependency
@@ -11,6 +13,8 @@ from core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 # for Hashing passwords
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# for decoding JWT
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def hash_password(*, password: str) -> str:
     """Create a hash from the given password"""
@@ -41,4 +45,22 @@ def create_access_token(*, user: Users, expires_delta: int = None) -> str:
             }
 
     return jwt.encode(to_encode, SECRET_KEY, algorithms=[ALGORITHM])
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    """Gets current user based on the decoded token"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise credentials_exception
+        return {"username": username, "user_id": user_id}
+
+    except JWTError:
+        raise credentials_exception
 
