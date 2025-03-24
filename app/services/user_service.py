7250e5962a003fa613d2a39eaa6b845e4b319927
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-24 14:09:37 +0330

    feat: user services - create access token
    Created a new service for generating access token for users

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,13 +1,32 @@
+from typing import Union
+from datetime import datetime, timedelta
+
+from jose import jwt
+
 from models.users import Users
 from dependencies import db_dependency
 from core.security import match_passwords
+from core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
-def authenticate_user(*, username: str, password: str, db: db_dependency) -> bool:
+def authenticate_user(*, username: str, password: str, db: db_dependency) -> Union[Users, bool]:
     """Authenticate user by the provided credentials"""
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
         return False
     if not match_passwords(main_password=user.password, entered_password=password):
         return False
-    return True
+    return user
+
+def create_access_token(*, user: Users, expires_delta: int = None) -> str:
+    """Creates JWT access token"""
+
+    to_encode = {
+            "sub": user.username,
+            "id": user.id,
+            "exp": datetime.utcnow() + timedelta(
+                minutes=expires_delta if expires_delta else ACCESS_TOKEN_EXPIRE_MINUTES
+                ),
+            }
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
