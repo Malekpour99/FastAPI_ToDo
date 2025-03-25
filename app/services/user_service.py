b4ce0f1a07960ed5ab8c3acfa9d9b6a2d228273a
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-25 12:14:16 +0330

    fix: user_service - get current user decoding & context
    Fixed algorithm error while decoding JWT access token, updated returned data and imported required dependency of oauth

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from jose import jwt, JWTError
 from fastapi import status, Depends, HTTPException
 
-from app.core.security import oauth2_scheme 
+from app.core.security import oauth2_scheme
 from app.core.config import SECRET_KEY, ALGORITHM
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
@@ -14,12 +14,12 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if username is None or user_id is None:
             raise credentials_exception
-        return {"username": username, "user_id": user_id}
+        return {"username": username, "id": user_id}
 
     except JWTError:
         raise credentials_exception
