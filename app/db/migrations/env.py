4475aac8a9978266f0bcbce6699726e16d5d65ab
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-28 15:39:48 +0330

    feat: alembic migrations - database configuration
    Read database URL from environment variables and included Base of the database in the alembic configuration

--- app/db/migrations/env.py
+++ app/db/migrations/env.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from app.db.database import Base
+
+load_dotenv()
+
+# Database
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +23,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+if SQLALCHEMY_DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
+else:
+    raise ValueError("SQLALCHEMY_DATABASE_URL is not set in the environment variables")
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
